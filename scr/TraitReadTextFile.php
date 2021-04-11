c2ca313112c3a89816220775e2e144dc0495f9fc
Steve Lewis <steve@themodulefactory.com>
2021-04-11 23:37:34 +0100

    Re-jigged the annotations and Namespace

--- scr/TraitReadTextFile.php
+++ scr/TraitReadTextFile.php
@@ -25,11 +25,33 @@ trait TraitReadTextFile {
         return explode(PHP_EOL, file_get_contents($srcFile));
     }
 
+    /**
+     * @param string $writePath
+     * @param string $textOutput
+     * @param string $fileOpenMode
+     */
     public function writeStringToFile (string $writePath = "", string $textOutput = "", string $fileOpenMode = "a"): void {
         if (!empty($writePath) && !empty($textOutput)) {
             $textOutputToFile = fopen($writePath, $fileOpenMode) or die("Unable to open file!");
 
             fwrite($textOutputToFile, $textOutput);
+
+            fclose($textOutputToFile);
+        }
+    }
+
+    /**
+     * @param string $writePath
+     * @param string $arrayOutput
+     * @param string $fileOpenMode
+     */
+    public function writeArrayToFile (string $writePath = "", string $arrayOutput = "", string $fileOpenMode = "a"): void {
+        if (!empty($writePath) && !is_null($arrayOutput)) {
+            $textOutputToFile = fopen($writePath, $fileOpenMode) or die("Unable to open file!");
+
+            foreach($arrayOutput as $key=>$arrayItem) {
+                fwrite($textOutputToFile, $arrayItem);
+            }
 
             fclose($textOutputToFile);
         }
