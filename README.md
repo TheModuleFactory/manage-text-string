c1e279a475981d50fba1f23c5be01a2cc618dfa3
Steve Lewis <steve@themodulefactory.com>
2021-04-13 10:59:31 +0100

    Repository reset and license change to MIT from

--- README.md
+++ README.md
@@ -5,3 +5,25 @@ A small package built for a private project to manage text read from a file from
 ## Installation:
 
 
+***
+### **DISCLAIMER**<br>
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+
+This package contains a copy of the MIT License available here;
+
+***
+### **LICENSE DETAILS**
+[https://github.com/TheModuleFactory/file-system-tree/blob/main/LICENSE](https://github.com/TheModuleFactory/file-system-tree/blob/main/LICENSE)
+
+For more information regarding Open Source Licensing you can visit;<br><br>
+[**https://www.mit.edu/~amini/LICENSE.md**](https://www.mit.edu/~amini/LICENSE.md)
+[**https://opensource.org/licenses/MIT**](https://opensource.org/licenses/MIT)
+
+***
+### **AUTHOR**
+**Steve Lewis** <steve@themodulefactory.com><br>
+[**Copyright 2021 &copy; The Module Factory Ltd.** Company Number; 09989173](https://www.themodulefactory.com)
--- scr/ManageTextString.php
+++ scr/ManageTextString.php
@@ -1,25 +1,27 @@
 <?php declare(strict_types=1);
 
 /**
- * @copyright 2021 &copy; Steve R. Lewis, The Module Factory Ltd.
+ * @copyright 2021© Steve Lewis, The Module Factory Ltd.
  *
- * @author Steve R. Lewis <steve@themodulefactory.com>
- * The Module Factory Ltd. Company Number; 09989173
- * https://www.themodulefactory.com
+ * @author Steve R. Lewis, <steve@themodulefactory.com>
  *
  * A small package built for a private project to manage text read from a file from which a text object is created.
  *
- * This program is free software: you can redistribute it and/or modify it under the terms of the
- * GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License,
- * or (at your option) any later version.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
- * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *
+ * You should have received a copy of the MIT License along with this program, alternatively you can get a copy here;
  * @license https://github.com/TheModuleFactory/file-system-tree/blob/main/LICENSE
- * See the GNU Affero General Public License Version 3 for more details.
- * You should have received a copy of the GNU Affero General Public License along with this program.
- * If not, see <https://www.gnu.org/licenses/>.
+ *
+ * For more information regarding Open Source Licensing you can visit;
+ *
+ *     <https://www.mit.edu/~amini/LICENSE.md>
+ *     <https://opensource.org/licenses/MIT>
+ *
+ * The Module Factory Ltd. Company Number; 09989173, <https://www.themodulefactory.com>
  */
 
 namespace ManageTextString;
